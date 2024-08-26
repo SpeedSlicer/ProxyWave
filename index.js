85734b3ec59acfb4026ba353d8cfabf7b7a7df4f
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2024-08-26 19:19:35 -0400

    chemical

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-(async() => {
-  await import('./index.mjs');
-})();
+import { ChemicalServer } from "chemicaljs";
+import express from "express";
+
+const chemical = new ChemicalServer();
+const port = process.env.PORT || 3000;
+
+chemical.use(express.static("public", {
+    index: "index.html",
+    extensions: ["html"]
+}));
+
+chemical.listen(port, () => {
+    console.log(`Chemical example styled listening on port ${port}`);
+});
